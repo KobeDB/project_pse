9dbba3e1d5f38d3c56805ed1f923d26078f3f067
emiel <emieldockx@icloud.com>
2022-04-17 18:22:30 +0200

    added reading of Voertuiggenerator in VerkeerssituatieReader.cpp added class for Voertuiggenerator edited gewone_verkeerssituatie.xml to use a VOERTUIGGENERATOR edited the Voertuiggenerator to use types

--- src/voertuigen/Bus.h
+++ src/voertuigen/Bus.h
@@ -10,14 +10,14 @@
 class Bus : public Voertuig {
 public:
     //TODO: deze constantes aanpassen
-    virtual float get_V_MAX() const { return 16.6f; }
+    virtual float get_V_MAX() const { return 11.4f; }
     virtual float get_VERTRAAGFACTOR() const { return 0.4f; }
-    virtual float get_REMFACTOR_MAX() const {return 4.61f;}
-    virtual float get_VERSNELLING_MAX() const {return 1.44f;}
+    virtual float get_REMFACTOR_MAX() const {return 4.29f;}
+    virtual float get_VERSNELLING_MAX() const {return 1.22f;}
     virtual int get_VERTRAAGAFSTAND() const {return 50;}
     virtual int get_STOPAFSTAND() const {return 15;}
-    virtual int get_MINIMALE_VOLGAFSTAND() const {return 4;}
-    virtual int get_VOERTUIG_LENGTE() const {return 4;}
+    virtual int get_MINIMALE_VOLGAFSTAND() const {return 12;}
+    virtual int get_VOERTUIG_LENGTE() const {return 12;}
 
     Bus(const std::string& baanNaam, int positie) : Voertuig(baanNaam, positie) {}
 
